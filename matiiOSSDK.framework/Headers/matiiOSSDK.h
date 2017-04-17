a5f3963742dd5a763ebd633ac90e6e683b547852
hovmush <hovmush@gmail.com>
2017-04-17 12:09:45 +0400

    Initial commit

---
+++ matiiOSSDK.framework/Headers/matiiOSSDK.h
@@ -0,0 +1,19 @@
+//
+//  matiiOSSDK.h
+//  matiiOSSDK
+//
+//  Created by Musho Hovhannisyan on 2/24/17.
+//  Copyright © 2017 Inomma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for matiiOSSDK.
+FOUNDATION_EXPORT double matiiOSSDKVersionNumber;
+
+//! Project version string for matiiOSSDK.
+FOUNDATION_EXPORT const unsigned char matiiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <matiiOSSDK/PublicHeader.h>
+
+
