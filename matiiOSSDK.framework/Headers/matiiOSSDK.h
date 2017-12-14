ceacf0c09d037e91388c0b07b7690ed107e7290c
Musho <hovmush@gmail.com>
2017-12-14 15:42:17 +0400

    SDK updated

--- matiiOSSDK.framework/Headers/matiiOSSDK.h
+++ matiiOSSDK.framework/Headers/matiiOSSDK.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double matiiOSSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char matiiOSSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <matiiOSSDK/PublicHeader.h>
-
-
